83b73470b5cd2d3da7234b2d47af8dc6d4fb6690
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-09-13 16:53:49 -0300

    filter

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+const people = [
+  {
+    id: "people-01",
+    title: "Juan perez",
+  },
+  {
+    id: "people-02",
+    title: "Marcos perez",
+  },
+  {
+    id: "people-03",
+    title: "Roman temes",
+  },
+  {
+    id: "people-04",
+    title: "Marcos dicesare",
+  },
+  {
+    id: "people-05",
+    title: "Ivan macedonio",
+  },
+];
+
+const calendar = [
+  {
+    id: "calendar-01",
+    title: "Viajar",
+  },
+  {
+    id: "calendar-02",
+    title: "Asistir a clase",
+  },
+  {
+    id: "calendar-03",
+    title: "Limpiar el auto",
+  },
+  {
+    id: "calendar-04",
+    title: "Modificar el codigo",
+  },
+  {
+    id: "calendar-05",
+    title: "Arreglar la pc",
+  },
+];
+
+function App() {
+  const [data, setData] = useState([...people, ...calendar]);
+
+  const [selection, setSelection] = useState(null);
+
+  const [currentOpction, setCurrentOption] = useState("all");
+
+  const handleClick = (e) => {
+    const op = e.target.name;
+
+    switch (op) {
+      case "all":
+        setData([...people, ...calendar]);
+        setCurrentOption("all");
+        break;
+      case "people":
+        setData([...people]);
+        setCurrentOption("people");
+        break;
+      case "calendar":
+        setData([...calendar]);
+        setCurrentOption("calendar");
+        break;
+
+      default:
+    }
+  };
+  return (
+    <>
+      <button onClick={handleClick} name="all">
+        All
+      </button>
+      <button onClick={handleClick} name="people">
+        People
+      </button>
+      <button onClick={handleClick} name="calendar">
+        Calendars
+      </button>
+      <div>
+        <h1>Current : {currentOpction}</h1>
+      </div>
+      <div>
+        {data.map(item=> <div>{item.title}</div>)}
+      </div>
+    </>
+  );
+}
+
+export default App;
